6f15ef1a7f001951028085d19e6b686242ecaf0b
MJ <142563498+MJw0lf@users.noreply.github.com>
2025-08-26 17:27:58 +1000

    UI score updates and prints game won if score is 3 or more
    As a player, I want to win once I have scored three times so that the game has and end goal

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,7 +1,11 @@
 extends Node
 
+var score : int = 0
 
 func _on_golf_hole_entered(body: Node3D) -> void:
 	if(body.name == "GolfBall"):
-		print_debug("Golf ball in hole")
 		body.position = Vector3(2.414, 0.668, -2.318)
+		score += 1
+		Hud.update_score(score)
+		if(score >= 3):
+			print_debug("Game won!")
