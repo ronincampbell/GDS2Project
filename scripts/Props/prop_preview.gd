95a9b6554309a9f3310737e59b21fd0eb0349f7c
MJ <142563498+MJw0lf@users.noreply.github.com>
2025-08-27 16:41:39 +1000

    Moves the plant preview before placing it :)
    As a player, I want to be able to place props before the game begins so that I can design the course how I want it

--- scripts/Props/prop_preview.gd
+++ scripts/Props/prop_preview.gd
@@ -3,9 +3,16 @@ extends Node3D
 @export var downward_raycasts: Array[RayCast3D]
 var prop: PackedScene = preload("res://Props/prop_plant.tscn")
 
-const speed : float = 0.05
+const speed : float = 5
 
 func _physics_process(delta: float) -> void:
+	
+	if Input.is_action_just_pressed("ui_accept"):
+		place_prop()
+	
+	_move_prop(delta)
+
+func _move_prop(delta: float) -> void:
 	if !downward_raycasts.is_empty():
 		var touching_ground = false
 		
@@ -14,13 +21,25 @@ func _physics_process(delta: float) -> void:
 				touching_ground = true
 		
 		if !touching_ground:
-			global_position += Vector3(0, -speed, 0)
+			position += Vector3(0, -1, 0)*delta*speed
 	
-	if Input.is_action_just_pressed("ui_accept"):
-		var new_prop = prop.instantiate()
-		new_prop.position = position
-		new_prop.rotation = rotation
-		print_debug(new_prop)
-		print_debug(new_prop.position)
-		get_parent().add_child(new_prop)
-		queue_free()
+	var direction: Vector3 = Vector3.ZERO
+	if Input.is_action_pressed("ui_left"):
+		direction += Vector3(-1, 0, 0)
+	if Input.is_action_pressed("ui_right"):
+		direction += Vector3(1, 0, 0)
+	if Input.is_action_pressed("ui_up"):
+		direction += Vector3(0, 0, -1)
+	if Input.is_action_pressed("ui_down"):
+		direction += Vector3(0, 0, 1)
+	
+	position += direction.normalized()*speed*delta
+
+func place_prop() -> void:
+	var new_prop = prop.instantiate()
+	new_prop.position = position
+	new_prop.rotation = rotation
+	#print_debug(new_prop)
+	#print_debug(new_prop.position)
+	get_parent().add_child(new_prop)
+	queue_free()
