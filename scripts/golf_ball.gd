dfe3e054699f6d21748f2e8357d42a53c0b749fc
CookieCherith0969 <cookiecherith@gmail.com>
2025-08-30 19:22:33 +1000

    Reorganising folders

--- scripts/golf_ball.gd
+++ scripts/golf_ball.gd
@@ -11,18 +11,6 @@ const max_spin_speed: float = 16
 var stored_aim_dir: Vector2 = Vector2.ZERO
 
 func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
-	"""if Input.is_action_just_pressed("ui_left"):
-		state.angular_velocity = Vector3.ZERO
-		apply_central_force(Vector3.LEFT*hit_power+Vector3.UP*lift_force)
-	if Input.is_action_just_pressed("ui_right"):
-		state.angular_velocity = Vector3.ZERO
-		apply_central_force(Vector3.RIGHT*hit_power+Vector3.UP*lift_force)
-	if Input.is_action_just_pressed("ui_up"):
-		state.angular_velocity = Vector3.ZERO
-		apply_central_force(Vector3.FORWARD*hit_power+Vector3.UP*lift_force)
-	if Input.is_action_just_pressed("ui_down"):
-		state.angular_velocity = Vector3.ZERO
-		apply_central_force(Vector3.BACK*hit_power+Vector3.UP*lift_force)"""
 	
 	var flat_aim_vector: Vector2 = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
 	if flat_aim_vector.is_zero_approx():
