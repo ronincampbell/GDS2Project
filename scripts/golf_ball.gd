affcbc9b5d3617ff83385d81c3f9bf30574d4900
CookieCherith0969 <cookiecherith@gmail.com>
2025-08-30 22:54:23 +1000

    Grabbing and swinging

--- scripts/golf_ball.gd
+++ scripts/golf_ball.gd
@@ -1,8 +1,8 @@
 class_name GolfBall
 extends RigidBody3D
 
-const hit_power: float = 200
-const lift_force: float = 200
+const hit_power: float = 5
+const lift_force: float = 5
 const max_move_speed: float = 10
 const max_spin_speed: float = 16
 
@@ -10,23 +10,35 @@ const max_spin_speed: float = 16
 @onready var aim_decal = $NoRotatePivot/YRotatePivot/AimDecal
 
 var stored_aim_dir: Vector2 = Vector2.ZERO
+var showing_aim_arrow: bool = false
 
 func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
-	var flat_aim_vector: Vector2 = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
-	if flat_aim_vector.is_zero_approx():
-		flat_aim_vector = stored_aim_dir
-	else:
-		stored_aim_dir = flat_aim_vector
-	
-	if Input.is_action_pressed("ui_accept"):
-		aim_decal.show()
-		y_rotate_pivot.rotation.y = -flat_aim_vector.angle()
-	else:
-		aim_decal.hide()
-	if Input.is_action_just_released("ui_accept") and !flat_aim_vector.is_zero_approx():
-		state.angular_velocity = Vector3.ZERO
-		var aim_vector: Vector3 = Vector3(flat_aim_vector.x, 0.0, flat_aim_vector.y)
-		apply_central_force(aim_vector*hit_power + Vector3.UP*lift_force)
-	
 	state.linear_velocity.limit_length(max_move_speed)
 	state.angular_velocity.limit_length(max_spin_speed)
+
+func show_aim_arrow():
+	showing_aim_arrow = true
+
+func hide_aim_arrow():
+	showing_aim_arrow = false
+	aim_decal.hide()
+	stored_aim_dir = Vector2.ZERO
+
+func get_3d_aim_dir():
+	return Vector3(stored_aim_dir.x, 0.0, stored_aim_dir.y)
+
+func aim_in_dir(direction: Vector3):
+	var flat_direction: Vector2 = Vector2(direction.x, direction.z)
+	if flat_direction.is_zero_approx():
+		return
+	aim_decal.show()
+	stored_aim_dir = flat_direction
+	
+	y_rotate_pivot.rotation.y = -flat_direction.angle()
+
+func launch_in_aim_direction():
+	if stored_aim_dir.is_zero_approx():
+		return
+	angular_velocity = Vector3.ZERO
+	var aim_vector: Vector3 = Vector3(stored_aim_dir.x, 0.0, stored_aim_dir.y)
+	apply_central_impulse(aim_vector*hit_power + Vector3.UP*lift_force)
--- scripts/golf_club.gd
+++ scripts/golf_club.gd
@@ -1,6 +1,17 @@
 class_name GolfClub
 extends RigidBody3D
 
+@onready var handle_marker: Marker3D = $HandleMarker
+@onready var head_marker: Marker3D = $HeadMarker
 
-func _on_area_3d_body_entered(body: Node3D) -> void:
-	pass # Replace with function body.
+func get_handle_force_offset():
+	return handle_marker.global_position - global_position
+
+func get_handle_global_pos():
+	return handle_marker.global_position
+
+func get_head_force_offset():
+	return head_marker.global_position - global_position
+
+func get_head_global_pos():
+	return head_marker.global_position
