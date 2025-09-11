7c8ddb3a552ed778d7063e4c7abcc1bb177ba7d0
vesanii <143512476+vesanii@users.noreply.github.com>
2025-09-11 11:24:58 +1000

    function to announce which player scored

--- UI/HUD/hud.gd
+++ UI/HUD/hud.gd
@@ -10,14 +10,29 @@ var player_count = 4
 	$PlayerScores/ScoreContainer3/ScoreMargin/ScoreText,
 	$PlayerScores/ScoreContainer4/ScoreMargin/ScoreText
 ]
+@onready var score_announce = $ScoreAnnouncePanel/AnnounceMargin/ScoreAnnounceText
 
 func _ready() -> void:
-	init_score_texts()
+	init_hud()
 
 func update_score(player_id: int, new_score: int):
 	player_scores.get(player_id).text = str(new_score)
 	
-func init_score_texts():
+func init_hud():
+	#reset score texts
+	reset_score_ui()
+	#reset score announcement
+	reset_score_announce()
+
+func announce_score(player_id: int):
+	score_announce.get_parent().get_parent().show()
+	score_announce.text = "Player " + str(player_id) + " Scored!"
+
+func reset_score_announce():
+	score_announce.get_parent().get_parent().hide()
+	score_announce.text = "Scored!"
+
+func reset_score_ui():
 	for player in player_count:
 		player_scores_container.get_child(player).show()
 		player_scores.get(player).text = str(0)
