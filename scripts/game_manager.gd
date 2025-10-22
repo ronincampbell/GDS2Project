7c61dcd1ef8d663fa3c2865d1312b4326043b8d7
CookieCherith0969 <cookiecherith@gmail.com>
2025-10-22 12:29:08 +1100

    Update game_manager.gd

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,14 +1,16 @@
 extends Node
 
-var score : int = 0
+var scores : Dictionary[int,int]
 
 @export
 var skip_placing: bool = false
 
 var mode : String = "obstacle placing"
 var obstacle_placing_timer : float = 0
-var obstacle_placing_time : float = 60                                                    
+var obstacle_placing_time : float = 20                                                    
 var player1_obstacle_in_scene : bool = false
+
+var match_start_time: float = 2.0
 
 const placeable_props: Dictionary = {"plant":preload("res://Props/Previews/prop_plant_preview.tscn"), "fertiliser":preload("res://Props/Previews/prop_fertiliser_preview.tscn"), "watering_can":preload("res://Props/Previews/prop_watering_can_preview.tscn")}
 const golf_ball: PackedScene = preload("res://CoreObjects/golf_ball.tscn")
@@ -25,8 +27,6 @@ const gnome_spawn_offset: Vector3 = Vector3(0,1.4,0)
 var players_in_scene: int = 1
 enum players_prop {PLAYER1, PLAYER2, PLAYER3, PLAYER4}
 
-@onready var camera_controller : Camera3D = $Camera3D
-
 var player_current_props = {
 	players_prop.PLAYER1: "plant",
 	players_prop.PLAYER2: "plant",
@@ -38,9 +38,17 @@ const prop_index = ["plant", "fertiliser", "watering_can"]
 var current_player_prop_index = [0, 0, 0, 0]
 var prop_preview_in_scene = [null, null, null, null]
 
+var active_players: Array[Node3D]
+var active_club: Node3D
+var active_ball: Node3D
+
 func _ready() -> void:
 	if ControllerManager.device_players.values().size() > 0:
 		players_in_scene = ControllerManager.device_players.values().size()
+		for player in ControllerManager.device_players.values():
+			scores[player] = 0
+		for i in players_in_scene:
+			Hud.update_score(i, 0)
 
 func _physics_process(delta: float) -> void:
 	if mode == "obstacle placing":
@@ -54,25 +62,22 @@ func _physics_process(delta: float) -> void:
 			prop_placement_ui.visible = false
 			mode = "playing"
 			for marker in get_tree().get_nodes_in_group("BallSpawnMarkers"):
-				marker.set_visibility(false)
-				_place_object(golf_ball, marker.global_position+ball_spawn_offset)
+				active_ball = _place_object(golf_ball, marker.global_position+ball_spawn_offset)
+				marker.hide()
 			for marker in get_tree().get_nodes_in_group("ClubSpawnMarkers"):
-				marker.set_visibility(false)
-				_place_object(golf_club, marker.global_position+club_spawn_offset)
+				active_club = _place_object(golf_club, marker.global_position+club_spawn_offset)
+				marker.hide()
 			var available_spawns: Array = get_tree().get_nodes_in_group("PlayerSpawnMarkers")
 			for player_num in ControllerManager.device_players.values():
 				var spawn_index: int = randi_range(0, available_spawns.size()-1)
 				var new_gnome = _place_object(gnome, available_spawns[spawn_index].global_position + gnome_spawn_offset)
 				new_gnome.player_num = player_num
-				new_gnome.add_to_group("Players")
-				available_spawns[spawn_index].set_visibility(false)
 				new_gnome.start_disable(match_start_time)
 				active_players.append(new_gnome)
 				PlayerManager.player_nodes.assign(active_players)
 				available_spawns.remove_at(spawn_index)
-			
-			for marker in available_spawns:
-				marker.set_visibility(false)
+			for marker in get_tree().get_nodes_in_group("PlayerSpawnMarkers"):
+				marker.hide()
 			
 			#_place_object(golf_club, Vector3(-0.6, 1.6, 0.4))
 			#var new_gnome = _place_object(gnome, Vector3(0, 1.4, 0))
@@ -83,7 +88,7 @@ func _physics_process(delta: float) -> void:
 		
 		for i in players_in_scene:
 			if !prop_preview_in_scene[i]:
-				_place_player_object(player_current_props[player_prop_index[i]], i+1, Vector3(0, 1.6, 0))
+				_place_player_object(player_current_props[player_prop_index[i]], i+1)
 			
 			var index_change = 0
 			if Input.is_action_just_pressed("NextObject"+str(i+1)):
@@ -92,8 +97,6 @@ func _physics_process(delta: float) -> void:
 				index_change -= 1
 			
 			if index_change != 0:
-				var pos : Vector3 = prop_preview_in_scene[i].position + Vector3(0, 1.6, 0)
-				
 				prop_preview_in_scene[i].queue_free()
 				
 				current_player_prop_index[i] += index_change
@@ -104,24 +107,25 @@ func _physics_process(delta: float) -> void:
 				
 				var new_prop = prop_index[current_player_prop_index[i]]
 				player_current_props[player_prop_index[i]] = new_prop
-				_place_player_object(new_prop, i, pos)
-				prop_placement_ui.update_selected(i, new_prop)
+				_place_player_object(new_prop, i+1)
+				prop_placement_ui.update_selected(i+1, new_prop)
 
-func _place_player_object(player_current_prop, player_num, pos) -> void:
-	var new_prop = _place_object(placeable_props[player_current_prop], pos)
+func _place_player_object(player_current_prop, player_num) -> void:
+	var new_prop = _place_object(placeable_props[player_current_prop], Vector3(0, 1.6, 0))
 	if new_prop.has_method("set_player"):
 		new_prop.set_player(player_num)
 	prop_preview_in_scene[player_num-1] = new_prop
 
 func _on_golf_hole_entered(body: Node3D) -> void:
-	if body.name == "GolfBall":
+	if body.name.contains("GolfBall"):
 		body.position = Vector3(2.414, 0.668, -2.318)
 		if body.has_method("reset_velocity"):
 			body.reset_velocity()
-		score += 1
-		Hud.update_score(0, score)
+		scores[body.last_hit_player] += 1
+		Hud.update_score(body.last_hit_player-1, scores[body.last_hit_player])
 		SoundPlayer.play_ball_in_hole()
-		if(score >= 3):
+		if(scores[body.last_hit_player] >= 3):
+			VictoryHoldover.last_winner = body.last_hit_player
 			body.queue_free()
 			print_debug("Game won!")
 			get_tree().change_scene_to_file.call_deferred("res://maps/victory_podium.tscn")
@@ -146,11 +150,7 @@ func _on_golf_hole_entered(body: Node3D) -> void:
 func _place_object(object: PackedScene, pos: Vector3) -> Node:
 	var new_object = object.instantiate()
 	new_object.position = pos
-	add_child(new_object)
-	
-	if camera_controller.has_method("update_targets"):
-		camera_controller.update_targets()
-	
+	add_child(new_object,true)
 	return new_object
 
 func prop_placed() -> void:
